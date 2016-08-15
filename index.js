ff6837fd5b1a9a1c819534aab75be72a173ffc7d
xiaodong <875697126@qq.com>
2016-08-15 19:00:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by lixiaodong on 16/8/15.
+ */
+module.exports = require('./lib/load');
---
+++ util/util.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lixiaodong on 16/8/15.
+ */
+var Util = function () {
+
+}
+
+var p = Util.prototype;
+
+p.getObjByKey = function () {
+
+}
+
+module.exports = new Util();
