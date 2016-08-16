871597c3468b6542c1e3e6ba735b031f03f7548b
xiaodong <875697126@qq.com>
2016-08-16 15:55:36 +0800

    add methods

---
+++ test.js
@@ -0,0 +1,13 @@
+var load = require('./lib/load');
+
+var model = new load({
+    configKey:{
+        person:['id','age']
+    }
+});
+
+model.loadConfig();
+//var obj = model.getObjByKey('person', 'id' , 1);
+//var objs = model.getObjArrayByKey('person', 'age', 18);
+//var obj = model.getObjFromArrayByKey('person', 'age', 18 , 'id' ,2)
+console.log(obj);
